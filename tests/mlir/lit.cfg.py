89849c6c1f4f144b1f34cef6dff794ce93224e51
Shuang Huang <nikshuang@sina.com>
2024-06-28 03:20:55 +0000

    add IR test: test_variable_decl

---
+++ tests/mlir/lit.cfg.py
@@ -0,0 +1,10 @@
+import os
+import lit
+
+config.name = "example"
+config.test_format = lit.formats.ShTest(True)
+config.suffixes = ['.py']
+config.test_source_root = os.path.dirname(__file__)
+config.test_exec_root = os.path.join(config.test_source_root, '/tmp')
+config.excludes = ['lit.cfg.py', 'test_lit.py']
+config.substitutions.append(['%FileCheck', '/usr/lib/llvm-15/bin/FileCheck'])
