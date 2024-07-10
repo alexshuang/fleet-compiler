207af2a622d30112e62cb9daffbd312066824a96
Shuang Huang <nikshuang@sina.com>
2024-07-10 08:09:46 +0000

    support parse matmul/pow binary ops

--- fleet_compiler/ir/dialects/tosa.py
+++ fleet_compiler/ir/dialects/tosa.py
@@ -30,3 +30,22 @@ class MulOp(_BinaryOp):
 
 
 class ReciprocalOp(_UnaryOp): ...
+
+
+class ReshapeOp(Operation):
+    def __init__(self, input: Value, attrs: dict[str, Attribute]):
+        attr = attrs['new_shape']
+        type = RankedTensorType(attr.value, input.type.element_type)
+        super().__init__(operands=[input], result_types=[type], attributes=attrs)
+
+
+class MatmulOp(Operation):
+    def __init__(self, lhs: Value, rhs: Value, attrs: dict[str, Attribute] = {}):
+        lhs_dims, rhs_dims = lhs.type.dims, rhs.type.dims
+        assert len(lhs_dims) == len(rhs_dims) == 2
+        if isinstance(lhs.type, RankedTensorType):
+            output_dims = [lhs_dims[0], rhs_dims[1]]
+            output_type = RankedTensorType(output_dims, lhs.type.element_type)
+        else:
+            output_type = UnrankedTensorType(lhs.type.element_type)
+        super().__init__(operands=[lhs, rhs], result_types=[output_type], attributes=attrs)
