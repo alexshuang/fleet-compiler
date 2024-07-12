dd08ef31755a4f56a43eed8c0a38e6e4424ae822
Shuang Huang <nikshuang@sina.com>
2024-07-12 10:20:36 +0800

    support parse layernorm function (#51)

--- fleet_compiler/ir/importer.py
+++ fleet_compiler/ir/importer.py
@@ -19,15 +19,16 @@ import numpy as np
 from functools import reduce
 import operator
 
+from typing import Union
 from .core import *
 from .builder import *
 from .dialects.builtin import *
 from .dialects.func import *
-from .dialects import arith, tosa, numpy as numpy_dialect, func, math
+from .dialects import arith, tosa, numpy as numpy_dialect, func, math, tensor
 
 
 from fleet_compiler.frontend.lexer import (
-    Op as opcode
+    Op as OpCode
 )
 
 from fleet_compiler.frontend.symbolic import (
@@ -162,22 +163,17 @@ class ConvertASTtoMLIR(AstVisitor):
         return value
 
     def visitBinary(self, node: Binary):
-        if node.op == opcode.Assign:
+        if node.op == OpCode.Assign:
             return self.visit(node.exp2)
-        elif node.op == opcode.Plus:
-            return self.make_add_op(node.exp1, node.exp2)
-        elif node.op == opcode.Minus:
-            return self.make_sub_op(node.exp1, node.exp2)
-        elif node.op == opcode.Multiply:
-            return self.make_mul_op(node.exp1, node.exp2)
-        elif node.op == opcode.Divide:
-            return self.make_div_op(node.exp1, node.exp2)
-        elif node.op == opcode.AT:
+        elif node.op in [OpCode.Plus, OpCode.Minus,
+                         OpCode.Multiply, OpCode.Divide]:
+            return self.make_elementwise_op(node.op, node.exp1, node.exp2)
+        elif node.op == OpCode.AT:
             return self.make_matmul_op(node.exp1, node.exp2)
-        elif node.op == opcode.Power:
+        elif node.op == OpCode.Power:
             return self.make_pow_op(node.exp1, node.exp2)
-        # elif node.op in [opcode.EQ, opcode.NE, opcode.GT,
-        #             opcode.GE, opcode.LT, opcode.LE]:
+        # elif node.op in [OpCode.EQ, OpCode.NE, OpCode.GT,
+        #             OpCode.GE, OpCode.LT, OpCode.LE]:
         #     return self.make_cmp_op(node.exp1, node.exp2)
         else:
             raise TypeError(f"Interpreter: Unsupport operator {node.op}")
@@ -254,58 +250,59 @@ class ConvertASTtoMLIR(AstVisitor):
         builder.insert(op)
         return op
 
-    def make_add_op(self, node1: Unary, node2: Unary):
+    def make_elementwise_op(self, opcode: OpCode, node1: Unary, node2: Unary):
         lhs = self.visit(node1)
         rhs = self.visit(node2)
-        if lhs.type != rhs.type:
-            raise ValueError(f"lhs.type != rhs.type: {lhs.type} vs. {rhs.type}")
-        if isinstance(lhs.type, IntegerType | BoolType):
-            return self.create(arith.AddIOp(lhs, rhs)).results[0]
-        elif isinstance(lhs.type, FloatType):
-            return self.create(arith.AddFOp(lhs, rhs)).results[0]
-        else:
-            return self.create(tosa.AddOp(lhs, rhs)).results[0]
 
-    def make_sub_op(self, node1: Unary, node2: Unary):
-        lhs = self.visit(node1)
-        rhs = self.visit(node2)
-        if lhs.type != rhs.type:
-            raise ValueError(f"lhs.type != rhs.type: {lhs.type} vs. {rhs.type}")
-        if isinstance(lhs.type, IntegerType | BoolType):
-            return self.create(arith.SubIOp(lhs, rhs)).results[0]
-        elif isinstance(lhs.type, FloatType):
-            return self.create(arith.SubFOp(lhs, rhs)).results[0]
-        else:
-            return self.create(tosa.SubOp(lhs, rhs)).results[0]
+        # boardcast scalar to tensor
+        if isinstance(lhs.type, Union[RankedTensorType | UnrankedTensorType]):
+            if not isinstance(rhs.type, Union[RankedTensorType | UnrankedTensorType]):
+                rhs = self.create(tensor.SplatOp(rhs, lhs.type)).results[0]
+        elif isinstance(rhs.type, RankedTensorType | UnrankedTensorType):
+            if not isinstance(lhs.type, Union[RankedTensorType | UnrankedTensorType]):
+                lhs = self.create(tensor.SplatOp(lhs, rhs.type)).results[0]
 
-    def make_mul_op(self, node1: Unary, node2: Unary):
-        lhs = self.visit(node1)
-        rhs = self.visit(node2)
-        if lhs.type != rhs.type:
-            raise ValueError(f"lhs.type != rhs.type: {lhs.type} vs. {rhs.type}")
-        if isinstance(lhs.type, IntegerType | BoolType):
-            return self.create(arith.MulIOp(lhs, rhs)).results[0]
-        elif isinstance(lhs.type, FloatType):
-            return self.create(arith.MulFOp(lhs, rhs)).results[0]
-        else:
-            return self.create(tosa.MulOp(lhs, rhs)).results[0]
-
-    def make_div_op(self, node1: Unary, node2: Unary):
-        lhs = self.visit(node1)
-        rhs = self.visit(node2)
-        if lhs.type != rhs.type:
-            raise ValueError(f"lhs.type != rhs.type: {lhs.type} vs. {rhs.type}")
-        if isinstance(lhs.type, IntegerType | BoolType):
-            return self.create(arith.DivSIOp(lhs, rhs)).results[0]
-        elif isinstance(lhs.type, FloatType):
-            return self.create(arith.DivFOp(lhs, rhs)).results[0]
-        else:
-            _rhs = self.create(tosa.ReciprocalOp(rhs)).results[0]
-            return self.create(tosa.MulOp(lhs, _rhs)).results[0]
+        if opcode == OpCode.Plus:
+            if isinstance(lhs.type, IntegerType | BoolType):
+                return self.create(arith.AddIOp(lhs, rhs)).results[0]
+            elif isinstance(lhs.type, FloatType):
+                return self.create(arith.AddFOp(lhs, rhs)).results[0]
+            else:
+                return self.create(tosa.AddOp(lhs, rhs)).results[0]
+        elif opcode == OpCode.Minus:
+            if isinstance(lhs.type, IntegerType | BoolType):
+                return self.create(arith.SubIOp(lhs, rhs)).results[0]
+            elif isinstance(lhs.type, FloatType):
+                return self.create(arith.SubFOp(lhs, rhs)).results[0]
+            else:
+                return self.create(tosa.SubOp(lhs, rhs)).results[0]
+        elif opcode == OpCode.Multiply:
+            if isinstance(lhs.type, IntegerType | BoolType):
+                return self.create(arith.MulIOp(lhs, rhs)).results[0]
+            elif isinstance(lhs.type, FloatType):
+                return self.create(arith.MulFOp(lhs, rhs)).results[0]
+            else:
+                return self.create(tosa.MulOp(lhs, rhs)).results[0]
+        elif opcode == OpCode.Divide:
+            if isinstance(lhs.type, IntegerType | BoolType):
+                return self.create(arith.DivSIOp(lhs, rhs)).results[0]
+            elif isinstance(lhs.type, FloatType):
+                return self.create(arith.DivFOp(lhs, rhs)).results[0]
+            else:
+                _rhs = self.create(tosa.ReciprocalOp(rhs)).results[0]
+                return self.create(tosa.MulOp(lhs, _rhs)).results[0]
 
     def make_numpy_op(self, op_name, *args, **kwargs):
         if op_name == 'numpy.random.randn':
-            return self.create(numpy_dialect.Random_RandnOp(args)).results[0]
+            return self.create(numpy_dialect.Random_RandnOp(args, kwargs)).results[0]
+        if op_name == 'numpy.transpose':
+            return self.create(numpy_dialect.TransposeOp(args, kwargs)).results[0]
+        if op_name == 'numpy.mean':
+            return self.create(numpy_dialect.MeanOp(args, kwargs)).results[0]
+        if op_name == 'numpy.var':
+            return self.create(numpy_dialect.VarOp(args, kwargs)).results[0]
+        if op_name == 'numpy.sqrt':
+            return self.create(numpy_dialect.SqrtOp(args, kwargs)).results[0]
         else:
             raise ValueError(f"unsupported op: {op_name}")
 
