329bd9e133b1de8e48975cc97bc9b7a8e7b51cfd
Shuang Huang <nikshuang@sina.com>
2024-07-29 18:50:28 +0800

    support numpy.sqrt lowering: (#63)
    - add pattern rewriter and pass manager
    - add LowerNumpyPass

---
+++ tests/mlir/test_lower_numpy.py
@@ -0,0 +1,15 @@
+# RUN: fleet_compiler_cli %s --emitMLIR --only-compile --opt | %FileCheck %s
+
+import numpy as np
+
+x = 20.0
+x2 = [20.0, 23.0, 43.0]
+b = np.sqrt(x)
+b2 = np.sqrt(x2)
+
+# CHECK: "builtin.module" () ({
+# CHECK-NEXT:   %0 = "arith.constant" () {value = 20.0: f32} : () -> f32
+# CHECK-NEXT:   %1 = "arith.constant" () {value = dense<[20.0, 23.0, 43.0]>: tensor<3xf32>} : () -> tensor<3xf32>
+# CHECK-NEXT:   %5 = "math.sqrt" (%0) : (f32) -> f32
+# CHECK-NEXT:   %4 = "math.sqrt" (%1) : (tensor<3xf32>) -> tensor<3xf32>
+# CHECK-NEXT: }) : () -> ()
