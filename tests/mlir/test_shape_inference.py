6fc5c9dd38af01c0fbf7c0366b41614b5b41652a
Shuang Huang <nikshuang@sina.com>
2024-08-04 02:32:41 +0000

    update test_shape_inference check data

--- tests/mlir/test_shape_inference.py
+++ tests/mlir/test_shape_inference.py
@@ -12,8 +12,6 @@ print(b)
 
 # CHECK: "builtin.module" () ({
 # CHECK-NEXT:   %2 = "arith.constant" () {value = 20.0: f32} : () -> f32
-# CHECK-NEXT:   %6 = "tensor.cast" (%2) : (f32) -> f32
-# CHECK-NEXT:   %7 = "math.sqrt" (%6) : (f32) -> f32
+# CHECK-NEXT:   %7 = "math.sqrt" (%2) : (f32) -> f32
 # CHECK-NEXT:   "python.print" (%7) : (f32) -> ()
 # CHECK-NEXT: }) : () -> ()
-
